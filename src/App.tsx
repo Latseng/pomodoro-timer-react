62ead9340e07829e639bcd1cbf33f3bb1f50e3fb
Lala Tseng <z162614@gmail.com>
2025-04-02 16:33:36 +0800

    Add dark mode and custom color in slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import { useState } from "react";
 import Timer from "./components/Timer";
 import TimerSettings from "./components/TimerSettings";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "./components/mode-toggle";
 
 function App() {
   const [isSettingsOpen, setIsSettingsOpen] = useState(true);
   return (
-    <main>
-      <h1 className="my-12 md:my-8 text-4xl font-bold text-center">pomodoro-timer</h1>
-      {isSettingsOpen ? <TimerSettings /> : <Timer />}
-    </main>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <main>
+        <ModeToggle />
+        <h1 className="my-12 md:my-8 text-4xl font-bold text-center">
+          pomodoro-timer
+        </h1>
+        {isSettingsOpen ? <TimerSettings /> : <Timer />}
+      </main>
+    </ThemeProvider>
   );
 }
 
--- src/components/TimerSettings.tsx
+++ src/components/TimerSettings.tsx
@@ -4,10 +4,26 @@ const TimerSettings = () => {
   return (
     <div className="mx-12">
       <h2 className="text-3xl font-semibold">Timer Settings</h2>
-      <label className="block">work minutes:</label>
-      <Slider defaultValue={[33]} max={100} step={1} />
-      <label className="block">break minutes:</label>
-      <Slider defaultValue={[33]} max={100} step={1} />
+      <div className="py-8 px-12 md:w-1/2 flex flex-col gap-4">
+        <div>
+          <h4 className="text-lg my-2">work minutes:</h4>
+          <Slider
+            defaultValue={[45]}
+            min={1}
+            max={120}
+            rangeColor="bg-red-500"
+          />
+        </div>
+        <div>
+          <h4 className="text-lg my-4">break minutes:</h4>
+          <Slider
+            defaultValue={[45]}
+            min={1}
+            max={120}
+            rangeColor="bg-green-500"
+          />
+        </div>
+      </div>
     </div>
   );
 };
