356cc49bbe2b6d0d22a0103b4e6629d34e96b0bb
Lala Tseng <z162614@gmail.com>
2025-04-02 11:23:52 +0800

    add a Timer with react-circular-progressbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import Timer from "./components/Timer";
+
 function App() {
 
   return (
     <main>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="my-12 text-3xl font-bold text-center">pomodoro-timer</h1>
+      <Timer />
     </main>
   );
 }
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,5 +1,23 @@
-export const Timer = () => {
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+
+const Timer = () => {
+  const percentage = 100;
+
   return (
-    <div>Timer</div>
-  )
-}
+    <div className="w-64 m-auto">
+      <CircularProgressbar
+        value={percentage}
+        text={`${percentage}%`}
+        styles={buildStyles({
+          rotation: 0.25,
+          strokeLinecap: "round",
+          textSize: "16px",
+          pathColor: `rgba(62, 152, 199, ${percentage / 100})`,
+        })}
+      />
+    </div>
+  );
+};
+
+export default Timer;
